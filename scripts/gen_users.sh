18e38592eb9f60e0ca76ce69aae774852aec8aa7
Justin Duplessis <drfoliberg@gmail.com>
2015-05-14 16:11:10 -0400

    tar.xz

--- scripts/gen_users.sh
+++ scripts/gen_users.sh
@@ -5,13 +5,32 @@ if [ -z "$1" ]; then
     exit 1;
 fi
 
-if [ "$(pwd)" != '/etc/openvpn' ]; then
-    echo "You should be in /etc/openvpn/"
+client="$1"
+owner="${2-$client}"
+
+if [ "$(pwd)" != "/etc/openvpn/easy-rsa" ]; then
+    echo "You should be in /etc/openvpn/easy-rsa/"
+    echo "Debian Wheezy: 'cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0/* /etc/openvpn/easy-rsa/'"
+    echo "Debian Jessie: 'apt-get install easy-rsa && cp -R /usr/share/easy-rsa/* /etc/openvpn/easy-rsa/'"
     exit 1
 fi
 
-client="$1"
+if [ ! -d "keys" ]; then
+    mkdir keys
+fi
 
-./build-key --batch
+if [ -f "keys/$client" ]; then
+    echo "Client $client already has a certificate"
+fi
 
-# TODO tar.xz keys and certs
+. vars > /dev/null
+
+./build-key --batch "$client"
+
+(cd keys && tar cfJ ../"$client".tar.xz ca.crt "$client".{key,crt})
+
+if [ ! id "$owner" ]; then
+    useradd -m -k "$owner"
+fi
+
+mv "$client".tar.xz /home/"$owner"/
